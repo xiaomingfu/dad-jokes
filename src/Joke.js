edaba366ed39a6520a2c0c644990794ec687ec22
Xiaoming Fu <xmingfu86@gmail.com>
2019-10-09 01:41:51 -0700

    styling vote

--- src/Joke.js
+++ src/Joke.js
@@ -2,20 +2,54 @@ import React, { Component } from 'react';
 import './Joke.css';
 
 class Joke extends Component {
+    changeColor() {
+        if (this.props.vote >= 15) {
+            return "#4CAF50";
+        } else if (this.props.vote >= 12) {
+            return "#8BC34A";
+        } else if (this.props.vote >= 9) {
+            return "#CDDC39"
+        } else if (this.props.vote >= 6) {
+            return "#FFEB3B"
+        } else if (this.props.vote >= 3) {
+            return "#FFC107";
+        } else if (this.props.vote >= 0) {
+            return "#FF9800";
+        } else {
+            return "#f44336";
+        }
+    }
+    changeEmoji() {
+        if (this.props.vote >= 15) {
+            return "rolling_on_the_floor_laughing";
+        } else if (this.props.vote >= 12) {
+            return "em em-laughing";
+        } else if (this.props.vote >= 9) {
+            return "em em-smiley";
+        } else if (this.props.vote >= 6) {
+            return "em em-slightly_smiling_face";
+        } else if (this.props.vote >= 3) {
+            return "em em-neutral_face";
+        } else if (this.props.vote >= 0) {
+            return "em em-confused";
+        } else {
+            return "em em-angry";
+        }
+    }
 
     render() {
         return (
             <div className="Joke">
                 <div className="Joke-btn">
                     <i className="fas fa-arrow-up" onClick={this.props.upvote}></i>
-                    <span className="Joke-vote">{this.props.vote}</span>
+                    <span className="Joke-vote" style={{ borderColor: this.changeColor() }}>{this.props.vote}</span>
                     <i className="fas fa-arrow-down" onClick={this.props.downvote}></i>
                 </div>
                 <div className="Joke-text">
                     {this.props.text}
                 </div>
                 <div className="Joke-smiley">
-                    <i className="em em-rolling_on_the_floor_laughing" aria-role="presentation" aria-label="ROLLING ON THE FLOOR LAUGHING"></i>
+                    <i className={this.changeEmoji()} aria-role="presentation" aria-label="ROLLING ON THE FLOOR LAUGHING"></i>
                 </div>
 
             </div>
